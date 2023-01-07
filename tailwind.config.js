96ee60e2a5a9ae1f8bd3c7f42fc8a8af8cf5ea8f
Elijan Mastnak <ejmastnak@gmail.com>
2023-01-07 22:02:59 +0100

    feat: improve layout on xs screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./layouts/**/*.html",
   ],
   theme: {
+    screens: {
+        'xs': '420px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     extend: {},
   },
   plugins: [
